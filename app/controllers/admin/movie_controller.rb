37cf8803c65cbc62de04cdc469e18faca424fec5
patriciouca <37183981+patriciouca@users.noreply.github.com>
2018-04-18 19:36:04 +0200

    Add files via upload

--- app/controllers/admin/movie_controller.rb
+++ app/controllers/admin/movie_controller.rb
@@ -17,21 +17,51 @@ def new
     end
   end
 
-  def edit
+    def edit
     load_data
     @movie = Movie.find(params[:id])
-    @page_title = 'Editar pelicula'
+    @page_title = 'Edit movie'
   end
 
   def update
     @movie = Movie.find(params[:id])
     if @movie.update_attributes(movie_params)
-      flash[:notice] = "Pelicula #{@movie.title} fue editada correctamente."
+      flash[:notice] = "Movie #{@movie.title} was succesfully updated."
       redirect_to :action => 'show', :id => @movie
     else
       load_data
-      @page_title = 'Editar pelicula'
+      @page_title = 'Edit movie'
       render :action => 'edit'
     end
   end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Succesfully deleted movie #{@movie.title}."
+    redirect_to :action => 'index'
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    @page_title = @movie.title
+  end
+
+  def index
+    sort_by = params[:sort_by]
+    @movies = Movie.order(sort_by).paginate(:page => params[:page], :per_page => 5)
+    @page_title = 'Listing movies'
+  end
+
+  private
+
+    def load_data
+      @directors = Director.all
+      @producers = Producer.all
+    end
+
+    def movie_params
+      params.require(:movie).permit(:title, :producer_id, :produced_at, { :director_ids => [] },
+                                   :serial_number, :blurb, :price, :length, :cover_image)
+    end
 end
