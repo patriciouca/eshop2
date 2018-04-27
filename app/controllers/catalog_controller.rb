a9439c16bd6aaa3c99a34a364f00d41dbed8d6a5
arantxaotal <37296889+arantxaotal@users.noreply.github.com>
2018-04-27 10:59:10 +0200

    Add files via upload

---
+++ app/controllers/catalog_controller.rb
@@ -0,0 +1,19 @@
+class CatalogController < ApplicationController
+  before_filter :initialize_cart, :except => :show
+  before_filter :require_no_user
+
+  def show
+    @movie = Movie.find(params[:id])
+    @page_title = @movie.title
+  end
+
+  def index
+    @movies = Movie.order("movies.id desc").includes(:directors, :producer).paginate(:page => params[:page], :per_page => 5)
+    @page_title = 'Catalogo'
+  end
+
+  def latest
+    @movies = Movie.latest 5 # invoques "latest" method to get the five latest movies
+    @page_title = 'Últimas peliculas'
+  end
+end
